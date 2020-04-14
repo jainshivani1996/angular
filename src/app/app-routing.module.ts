8f98aae4c651c11695efa64d8f7368d3f64edf6d
Shivani Jain <jainshivani.1996@gmail.com>
2020-04-14 15:17:46 +0530

    Final Commit 1.0

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import{AboutComponent}from './about/about.component';
+import{ContactComponent}from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { UtilityComponent } from './utility/utility.component';
+import { EmployeeComponent } from './employee/employee.component';
+const routes: Routes = [{path:'',component:HomeComponent},
+{path: 'about',component:AboutComponent},
+{path: 'contact',component:ContactComponent},
+{path: 'team',component:TeamComponent},
+{path: 'utility',component:UtilityComponent},
+{path: 'employee',component:EmployeeComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
